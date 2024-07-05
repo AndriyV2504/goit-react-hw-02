abcba85c7c394f93b422f0794392b98a0e5b3509
AndriyV2504 <andreyvolyanik@gmail.com>
2024-07-05 23:55:04 +0300

    add_hw_2_components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,16 @@
+import React from "react";
+import Description from "./components/Description/Description";
+import Options from "./components/Options/Options";
+import Feedback from "./components/Feedback/Feedback";
+import Notification from "../components/Notification/Notification";
+
 const App = () => {
   return (
     <div>
-      <h1>Welcome</h1>
+      <Description />
+      <Options />
+      <Feedback />
+      <Notification />
     </div>
   );
 };
