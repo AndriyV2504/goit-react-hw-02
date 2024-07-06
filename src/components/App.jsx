1e49403d1ed28286c04463c84e935a9df53012f4
AndriyV2504 <andreyvolyanik@gmail.com>
2024-07-06 11:05:55 +0300

    feedback_App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,38 @@ import Description from "./Description/Description";
 import Options from "./Options/Options";
 import Feedback from "./Feedback/Feedback";
 import Notification from "./Notification/Notification";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const App = () => {
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  });
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
   return (
     <div>
       <Description />
-      <Options />
-      <Feedback />
-      <Notification />
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          feedback={feedback}
+          totalFeedback={totalFeedback}
+          positiveFeedbackPercent={positiveFeedbackPercent}
+        />
+      ) : (
+        <Notification message="No feedback given" />
+      )}
     </div>
   );
 };
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,5 +1,13 @@
-const Feedback = () => {
-  return <div></div>;
+const Feedback = ({ feedback, totalFeedback, positiveFeedbackPercent }) => {
+  return (
+    <div>
+      <p>Good: {feedback.good}</p>
+      <p>Neutral: {feedback.neutral}</p>
+      <p>Bad: {feedback.bad}</p>
+      <p>Total Feedback: {totalFeedback}</p>
+      <p>Positive Feedback: {positiveFeedbackPercent}%</p>
+    </div>
+  );
 };
 
 export default Feedback;
